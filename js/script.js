15eb6cc297d332fc9f734bb0416f0f0d3bae640e
moniquetrotta <monique.trotta@gmail.com>
2021-07-18 19:27:18 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function salvarEmail (){
+  if(typeof(Storage) !== "undefined") {
+    
+  if (localStorage.cont) {
+    localStorage.cont = Number (localStorage.cont)+1;
+  }
+  else {
+    localStorage.cont = 1;
+  }
+  
+  Email = document.getElementById('email').value;
+  localStorage.setItem("Email" +localStorage.cont, Email);
+  } else {
+  
+  }
+  
+}
